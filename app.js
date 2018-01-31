665def6b65650f452e21b9f24c0b9c6e8d849277
Scott Barclay <scottdbarclay@gmail.com>
2018-01-31 11:08:38 -0800

    Add grocery list component with grocery item components inside

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+// create groceryList component
+// var GroceryList = () => (
+//   <ul><li>almonds</li><li>cranberries</li></ul>
+// );
+
+
+var Almonds = () => (
+  <li>almonds</li>
+);
+
+var Cranberries = () => (
+  <li>cranberries</li>
+);
+
+var GroceryList = () => (
+  <div>
+    <ul>
+	    <h2>Grocery List</h2>
+			    <Almonds />
+			    <Cranberries />
+    </ul>
+  </div>
+);
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
