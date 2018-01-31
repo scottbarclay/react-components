ffeb2c32ff91dd2ed7b482a4d69a34098eb52614
Scott Barclay <scottdbarclay@gmail.com>
2018-01-31 13:19:30 -0800

    Add grocery list as a class

--- app.js
+++ app.js
@@ -5,21 +5,71 @@
 // );
 
 
-var Almonds = () => (
-  <li>almonds</li>
-);
+// var Almonds = () => (
+//   <li>almonds</li>
+// );
 
-var Cranberries = () => (
-  <li>cranberries</li>
-);
+// var Cranberries = () => (
+//   <li>cranberries</li>
+// );
 
-var GroceryList = () => (
-  <div>
-    <ul>
-	    <h2>Grocery List</h2>
-			    <Almonds />
-			    <Cranberries />
-    </ul>
+// var GroceryList = () => (
+//   <div>
+//     <h2>Grocery List</h2>
+// 	  <GroceryListItem groceryItems={['almonds', 'cranberries']} />
+//   </div>
+// );
+
+// var groceryItems = ['almonds', 'cranberries'];
+
+var GroceryList = (props) => (
+	<div>
+		<h2>Grocery List</h2>
+		<ul>		  
+	    {props.groceryItems.map(item =>
+	      <GroceryListItem item={item} />
+	    )}		  
+	  </ul>
   </div>
 );
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.groceryItems[0]}</li>
+//     <li>{props.groceryItems[1]}</li>
+//   </ul>
+// );
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+      <li>{this.props.item}</li>
+		);
+	}
+}
+
+ReactDOM.render(<GroceryList groceryItems={['almonds', 'cranberries']}/>, document.getElementById("app"));
+
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <li>{this.props.todo}</li>
+//     );
+//   }
+// }
+
+// // Update our `TodoList` to use the new `TodoListItem` component
+// // This can still be a stateless function component!
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo =>
+//       <TodoListItem todo={todo} />
+//     )}
+//   </ul>
+// );
